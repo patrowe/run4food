82d520fc22012a7b8c356fa64ce6b13c76b1b9e1
patrowe <pamela.trowe@gmail.com>
2019-05-14 18:27:29 +0200

    jetzt mit geändertem classpath

---
+++ run4food/src/module-info.java
@@ -0,0 +1,10 @@
+module fun4food { 
+	requires javafx.base; 
+	requires javafx.controls; 
+	requires javafx.fxml; 
+	requires javafx.graphics; 
+	requires javafx.media; 
+	requires javafx.swing; 
+	requires javafx.web;
+	exports run4food; 
+}
